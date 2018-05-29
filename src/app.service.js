f174311ff24f851af467861f81b4b84f5e88fff4
Oshan Kottege <oshan.kottege@myob.com>
2018-05-30 09:16:56 +1200

    Got the authentication working.

--- src/app.service.js
+++ src/app.service.js
@@ -1,11 +1,32 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.fullstackweekly.com';
+axios.interceptors.request.use((config) => {
+  if (typeof window === 'undefined') {
+    return config;
+  }
+
+  const token = window.localStorage.getItem('token');
+  const newConfig = config;
+  if (token) {
+    newConfig.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return newConfig;
+});
 
 const appService = {
   getPosts(categoryId) {
     return new Promise((resolve) => {
       axios.get(`/wp-json/wp/v2/posts?categories=${categoryId}&per_page=6`)
+        .then((resp) => {
+          resolve(resp.data);
+        });
+    });
+  },
+  getProfile() {
+    return new Promise((resolve) => {
+      axios.get('/services/profile.php')
         .then((resp) => {
           resolve(resp.data);
         });
--- src/theme/Login.vue
+++ src/theme/Login.vue
@@ -1,40 +1,50 @@
 <template>
 <div class="content">
-  <h2>Login</h2>
-  <div class="field is-horizontal">
-    <div class="field-label is-normal">
-      <label class="label">Username</label>
-    </div>
-    <div class="field-body">
-      <div class="field">
-        <div class="control">
-          <input v-model="username" class="input" type="text" placeholder="Your username">
+  <div v-if="isAuthenticated">
+    <h1>Hello authenticated user</h1>
+    <p>Name: {{ profile.firstName }}</p>
+    <p>Favorite Sandwich: {{ profile.favoriteSandwich }}</p>
+    <button v-on:click="logout()" class="button is-primary">
+      Logout
+    </button>
+  </div>
+  <div v-else>
+    <h2>Login</h2>
+    <div class="field is-horizontal">
+      <div class="field-label is-normal">
+        <label class="label">Username</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <div class="control">
+            <input v-model="username" class="input" type="text" placeholder="Your username">
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  <div class="field is-horizontal">
-    <div class="field-label is-normal">
-      <label class="label">Password</label>
-    </div>
-    <div class="field-body">
-      <div class="field">
-        <div class="control">
-          <input v-bind="password" class="input" type="password" placeholder="Your password">
+    <div class="field is-horizontal">
+      <div class="field-label is-normal">
+        <label class="label">Password</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <div class="control">
+            <input v-model="password" class="input" type="password" placeholder="Your password">
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  <div class="field is-horizontal">
-    <div class="field-label">
-      <!-- Left empty for spacing -->
-    </div>
-    <div class="field-body">
-      <div class="field">
-        <div class="control">
-          <button v-on:click="login()" class="button is-primary">
-            Login
-          </button>
+    <div class="field is-horizontal">
+      <div class="field-label">
+        <!-- Left empty for spacing -->
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <div class="control">
+            <button v-on:click="login()" class="button is-primary">
+              Login
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -49,7 +59,21 @@ export default {
     return {
       username: '',
       password: '',
+      isAuthenticated: false,
+      profile: {},
     };
+  },
+  watch: {
+    isAuthenticated(val) {
+      if (val) {
+        appService.getProfile()
+          .then((prof) => {
+            this.profile = prof;
+          });
+      } else {
+        this.profile = {};
+      }
+    },
   },
   methods: {
     login() {
@@ -57,8 +81,24 @@ export default {
         .then((data) => {
           window.localStorage.setItem('token', data.token);
           window.localStorage.setItem('tokenExpiration', data.expiration);
+          this.isAuthenticated = true;
+
+          this.username = '';
+          this.password = '';
         }).catch(() => window.alert('Could not Login!'));
     },
+    logout() {
+      window.localStorage.setItem('token', null);
+      window.localStorage.setItem('tokenExpiration', null);
+      this.isAuthenticated = false;
+    },
+  },
+  created() {
+    const expiration = window.localStorage.getItem('tokenExpiration');
+    const timestamp = new Date().getTime() / 1000;
+    if (expiration !== null && (parseInt(expiration, 10) - timestamp) > 0) {
+      this.isAuthenticated = true;
+    }
   },
 };
 </script>
