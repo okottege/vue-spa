942c802427fcd3605f026410a8550d0069d0d3b0
Oshan Kottege <oshan.kottege@myob.com>
2018-06-01 16:16:16 +1200

    Added npm scripts to get ready for deployment.

--- server.js
+++ server.js
@@ -5,15 +5,23 @@ const path = require('path');
 
 const app = express();
 
+const isProd = typeof process.env.NODE_ENV !== 'undefined' && (process.env.NODE_ENV === 'production');
+
 const indexHTML = (() => (
   fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8')
 ))();
 
 const port = process.env.PORT || 3001;
 
-app.use('/dist', express.static(path.resolve(__dirname, './dist')));
+if (isProd) {
+  app.use('/', express.static(path.resolve(__dirname, './dist')));
+} else {
+  app.use('/dist', express.static(path.resolve(__dirname, './dist')));
+}
 
-require('./build/dev-server')(app);
+if (!isProd) {
+  require('./build/dev-server')(app);
+}
 
 app.get('*', (req, res) => {
   res.write(indexHTML);
